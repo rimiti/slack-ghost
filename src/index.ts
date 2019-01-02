4792395d8ec4503343fd245a39ced33791c3cf0f
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-01-02 18:51:16 +0100

    refact(index.ts) small refact, code improved

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@
 
 import { RTMClient } from "@slack/client";
 
-if (!process.env.SLACK_GHOST_TOKEN) {
+if (process.env.SLACK_GHOST_TOKEN.length === undefined) {
   throw new Error(`"SLACK_GHOST_TOKEN" environment variable must be set.`);
 }
 
 const rtm = new RTMClient(process.env.SLACK_GHOST_TOKEN);
 let connected: boolean = false;
 let isFirstConnection: boolean = true;
+
+/**
+ * @description IStartResponse interface.
+ */
+interface IStartResponse {
+  self: {
+    id: string;
+    name: string;
+  }
+}
 
 /**
  * @description Connect user an simulate activity to appear as connected.
@@ -19,15 +29,14 @@ async function connection() {
       if(!isFirstConnection) {
         await rtm.disconnect();
       }
-      //@ts-ignore
-      const {self} = await rtm.start();
-
-      await rtm.subscribePresence(self.id);
+      // @ts-ignore
+      const data:IStartResponse = await rtm.start();
+      await rtm.subscribePresence([data.self.id]);
       connected = rtm.connected;
 
       if(isFirstConnection) {
         isFirstConnection = false;
-        console.log(`[${new Date().toISOString()}] - You are connected as "${self.name}" (${self.id}).`)
+        console.log(`[${new Date().toISOString()}] - You are connected as "${data.self.name}" (${data.self.id}).`)
       } else {
         console.log(`[${new Date().toISOString()}] - Your presence has been refreshed.`)
       }
