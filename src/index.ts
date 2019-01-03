7cfb77f76c42e412aaa14991882f2f36ba03c083
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-01-03 15:35:40 +0100

    feat(index.ts) stop and restart connection every 28 minutes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-
 import { RTMClient } from "@slack/client";
+import * as moment from "moment";
 
-if (process.env.SLACK_GHOST_TOKEN.length === undefined) {
+if (process.env.SLACK_GHOST_TOKEN === undefined) {
   throw new Error(`"SLACK_GHOST_TOKEN" environment variable must be set.`);
 }
 
 const rtm = new RTMClient(process.env.SLACK_GHOST_TOKEN);
 let data: IStartResponse;
 let isFirstConnection: boolean = true;
-
+let lastConnection: moment.Moment;
 /**
  * @description IStartResponse interface.
  */
@@ -22,21 +22,28 @@ interface IStartResponse {
 
 /**
  * @description Connect user an simulate activity to appear as connected.
+ * The connection is automatically stopped and restart every 28 minutes.
  */
 async function connection() {
   try {
-    if(isFirstConnection) {
+    if(!isFirstConnection && moment().diff(lastConnection, "seconds") >= 28) {
+      lastConnection = moment();
+      await rtm.disconnect();
       // @ts-ignore
       data = await rtm.start();
+      console.log(`[${moment().format()}] - Connexion auto restarted.`);
     }
-    await rtm.subscribePresence([data.self.id]);
 
     if(isFirstConnection) {
       isFirstConnection = false;
-      console.log(`[${new Date().toISOString()}] - You are connected as "${data.self.name}" (${data.self.id}).`)
+      lastConnection = moment();
+      // @ts-ignore
+      data = await rtm.start();
+      console.log(`[${moment().format()}] - You are connected as "${data.self.name}" (${data.self.id}).`)
     }
+    await rtm.subscribePresence([data.self.id]);
   } catch (e) {
-    console.error(`[${new Date().toISOString()}] - Unable to connect you to the Slack servers:`, e);
+    console.error(`[${moment().format()}] - Unable to connect you to the Slack servers:`, e);
   }
 }
 
